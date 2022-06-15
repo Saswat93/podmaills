941fdefce87ab820451b1977e78792e0cffc178a
Suman Dinda <sumandinda123@gmail.com>
2022-06-15 17:57:54 +0530

    changed time to 6 hours ago

--- podmail.sh
+++ podmail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-output=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}} {{.status.phase}} {{.metadata.creationTimestamp}}{{"\n"}}{{end}}' | grep Pending | awk '$3 <= "'$(date -d '2 hours ago' -Ins --utc | sed 's/+0000/Z/')'" { print $1 }')
+output=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}} {{.status.phase}} {{.metadata.creationTimestamp}}{{"\n"}}{{end}}' | grep Pending | awk '$3 <= "'$(date -d '6 hours ago' -Ins --utc | sed 's/+0000/Z/')'" { print $1 }')
 lcount=$(echo $output | tr ' ' '\n' | wc -l)
 if [ $lcount > 0 ]; then
     echo -e $output | tr ' ' '\n' | mail -s "Pending Pod List > 6 hours" saspattn@cisco.com
